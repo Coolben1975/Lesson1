61ad1dc3ae8bc048db0df241b0f155e0f0c6c724
CoolBenWork <ben1@yandex.ru>
2023-02-14 10:23:15 +0300

    edit task1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿int number;
-Console.Write("Введите число: ");
+﻿int number1;
+int number2;
+Console.Write("Введите первое число: ");
 
-int.TryParse(Console.ReadLine()!, out number);
+int.TryParse(Console.ReadLine()!, out number1);
+Console.Write("Введите второе число: ");
 
-Console.Write($"{number}^2 = {number * number}");
+int.TryParse(Console.ReadLine()!, out number2);
+
+if (number1 < number2)
+	Console.Write($"max = {number2}");
+
+else
+	Console.Write($"max = {number1}");
