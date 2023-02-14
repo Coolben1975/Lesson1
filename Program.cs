4f3908846143d3b5b851be79bd864bc703b8dc86
CoolBenWork <ben1@yandex.ru>
2023-02-14 12:01:12 +0300

    edit task1 namber6

--- Program.cs
+++ Program.cs
@@ -40,4 +40,16 @@ for (int i = 1; i <= 3; i++)
 Console.Write($"max = {max}");
 */
 
+/* -------------- Задача 6: 
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+*/
+
+int number;
+Console.Write("Введите число: ");
+
+int.TryParse(Console.ReadLine()!, out number);
+if (number % 2 == 0)
+	Console.Write($"{number} -> Да");
+else
+	Console.Write($"{number} -> Нет");
