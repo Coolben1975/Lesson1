9754882c6127958f7eed22f0b97a6c6c1bf35804
CoolBenWork <ben1@yandex.ru>
2023-02-14 11:12:44 +0300

    edit task1 namber4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿int number1;
+﻿/* -------------- Задача 2:
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+/* 
+int number1;
 int number2;
 Console.Write("Введите первое число: ");
 
@@ -12,3 +20,24 @@ if (number1 < number2)
 
 else
 	Console.Write($"max = {number1}");
+*/
+
+/* ------------ Задача 4:
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+int number, max = 0;
+
+for (int i = 1; i <= 3; i++)
+{
+	Console.Write($"Введите число - {i} подтверждаете <Enter>: ");
+	int.TryParse(Console.ReadLine()!, out number);
+
+	if (number > max)
+		max = number;
+}
+
+Console.Write($"max = {max}");
+*/
+
+
