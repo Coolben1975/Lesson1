e9ef4517fa8a57d7ed10de43919b9763f989049c
Coolben1975 <ben1@yandex.ru>
2023-02-19 10:41:33 +0300

    add task3

--- Program.cs
+++ Program.cs
@@ -1,22 +1,6 @@
-﻿/*Console.Write("Введите число: ");
-string text = Console.ReadLine()!; // можно сократить передав сразу в TryParse вместо текста
-//int number = int.Parse(text); ниже сделаем правильнее
-int number;
-int.TryParse(text, out number);
-
-//Console.Write(number);
-//Console.Write("^2 = ");
-//Console.Write(number * number);
-Console.Write($"{number}^2 = {number * number}");*/
-
-int number;
+﻿int number;
 Console.Write("Введите число: ");
-//string text = Console.ReadLine()!; // можно сократить передав сразу в TryParse вместо текста
-//int number = int.Parse(text); ниже сделаем правильнее
 
 int.TryParse(Console.ReadLine()!, out number);
 
-//Console.Write(number);
-//Console.Write("^2 = ");
-//Console.Write(number * number);
 Console.Write($"{number}^2 = {number * number}");
