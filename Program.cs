ebf45f50eeb8757029a787a6ebc5ccf4cdcd10f4
Coolben1975 <ben1@yandex.ru>
2023-03-04 02:53:09 +0300

    Change task5

--- Program.cs
+++ Program.cs
@@ -1,110 +1,113 @@
-﻿/* Найти сумму положительных и отрицательных элементов массива
-Например: [12 элементов] */
+﻿/*---------- Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
 
-/*
-int[] array = FillArray(12, -9, 10);
-Print(array);
-Console.Write($"{String.Join(", ", array)}\n"); // вывод с String.Join
+// int[] array = FillArray(4, 100, 1000);
+// Print(array);
+// PrintChet(array);
 
-int pos = 0;
-int neg = 0;
-
-// for (int i = 0; i < array.Length; i++)  // вывод с for
+// void PrintChet(int[] arr)
 // {
-//  if (array[i] > 0)
-//      pos += array[i];
-//  else
-//      neg += array[i];
-
+// 	int sum = 0;
+// 	for (int i = 0; i < arr.Length; i++)
+// 	{
+// 		if (arr[i] % 2 == 0)
+// 			sum += 1;
+// 	}
+// 	Console.Write($"{sum}");
 // }
 
-foreach (int el in array)  // вывод с foreach но он не меняет значения массива
+// void Print(Array arr)
+// {
+// 	Console.Write($"[");
+// 	Console.Write($"{String.Join(", ", array)}] -> ");
+// }
+
+// int[] FillArray(int size, int min = 100, int max = 1000)
+// {
+// 	int[] arr = new int[size];
+// 	for (int i = 0; i < size; i++)
+// 		arr[i] = new Random().Next(min, max);
+// 	return arr;
+// }
+
+/*-----------Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+*/
+
+// int[] array = FillArray(4, -99, 100);
+// Print(array);
+// Console.Write(PrintSum(array));
+
+// int PrintSum(int[] arr)
+// {
+// 	int sum = 0;
+// 	for (int i = 0; i < arr.Length; i++)
+// 	{
+// 		if (i % 2 != 0)
+// 			sum = sum + arr[i];
+// 	}
+// 	return sum;
+// }
+
+// void Print(Array arr)
+// {
+// 	Console.Write($"[");
+// 	Console.Write($"{String.Join(", ", array)}] -> ");
+// }
+
+// int[] FillArray(int size, int min = -99, int max = 100)
+// {
+// 	int[] arr = new int[size];
+// 	for (int i = 0; i < size; i++)
+// 		arr[i] = new Random().Next(min, max);
+// 	return arr;
+// }
+
+
+
+/* Задача 38: Задайте массив !!! НАТУРАЛЬНЫХ как я вижу!!!!!! чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+
+int[] array = FillArray(5, 0, 100);
+Print(array);
+
+int min = SendIndex(array, 0);
+int max = SendIndex(array, 1);
+Console.Write($"{max - min}");
+
+int SendIndex(int[] array, int index)
 {
-	pos += el > 0 ? el : 0;  //тернарный оператор вместо if
-	neg += el < 0 ? el : 0;
-	// if (el > 0)
-	//  pos += el;
-	// else
-	//  neg += el;
+	int ind = index;
+	if (index == 0)
+	{
+		foreach (int el in array)
+		{
+			if (ind < el)
+				ind = el;
+		}
+
+	}
+	if (index == 1)
+	{
+		foreach (int el in array)
+		{
+			if (ind > el)
+				ind = el;
+		}
+
+	}
+	return ind;
 }
-
-Console.Write($"pos sum = {pos}\nneg sum = {neg}");
-
-// double[] array2 = new double[12]; // массив с double
-// Print(array2);                    // массив с double
 
 void Print(Array arr)
 {
-	for (int i = 0; i < arr.Length; i++)
-		Console.Write($"{arr.GetValue(i)} ");
-	Console.WriteLine();
+	Console.Write($"[");
+	Console.Write($"{String.Join(", ", array)}] -> ");
 }
 
-int[] FillArray(int size, int min = 0, int max = 10)
+int[] FillArray(int size, int min = 0, int max = 100)
 {
 	int[] arr = new int[size];
 	for (int i = 0; i < size; i++)
 		arr[i] = new Random().Next(min, max);
 	return arr;
 }
-*/
-
-/* Дан массив, изменить положительные элементы на отрицательные и наоборот
-[-4, -8, 8, 2] -> [4, 8, -8, -2] */
-
-// int[] array = FillArray(12, -9, 10);
-
-// Console.Write($"{String.Join(", ", array)}\n"); // вывод с String.Join
-// InverseElements(array);
-// Console.Write($"{String.Join(", ", array)}\n");
-
-// void InverseElements(int[] arr)
-// {
-// 	for (int i = 0; i < array.Length; i++)
-// 		arr[i] *= -1;
-// }
-
-// int[] FillArray(int size, int min = 0, int max = 10)
-// {
-// 	int[] arr = new int[size];
-// 	for (int i = 0; i < size; i++)
-// 		arr[i] = new Random().Next(min, max);
-// 	return arr;
-// }
-
-
-
-/* Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-Результат – в новом массиве.
-[1 2 3 4 5] -> 5 8 3
-[6 7 3 6] -> 36 21 */
-
-
-// int[] array = FillArray(7, 0, 10);
-// int[] pairs = new int[array.Length / 2 + array.Length % 2];
-
-// Console.Write($"{String.Join(", ", array)}\n");
-// MuliplyPairs(array, pairs);
-
-// Console.Write($"{String.Join(", ", pairs)}\n");
-// void MuliplyPairs(int[] arr, int[] pair)
-// {
-// 	int size = arr.Length;
-// 	int pairSize = pair.Length;
-
-// 	for (int i = 0; i < size / 2; i++)
-// 	{
-// 		int rev = size - i - 1;
-// 		pair[i] = arr[i] * arr[rev];
-// 	}
-// 	if (size % 2 != 0)
-// 		pair[pairSize - 1] = arr[pairSize - 1];
-// }
-
-// int[] FillArray(int size, int min = 0, int max = 10)
-// {
-// 	int[] arr = new int[size];
-// 	for (int i = 0; i < size; i++)
-// 		arr[i] = new Random().Next(min, max);
-// 	return arr;
-// }
