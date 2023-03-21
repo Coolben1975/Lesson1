721e45a4b1b22963e50093a4c8817e1c1c1421fc
CoolBenWork <ben1@yandex.ru>
2023-03-21 15:27:33 +0300

    changes master less-7

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-﻿/*Console.Write("Введите число: ");
-string text = Console.ReadLine()!; // можно сократить передав сразу в TryParse вместо текста
-//int number = int.Parse(text); ниже сделаем правильнее
-int number;
-int.TryParse(text, out number);
+﻿/*-----------------Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
 
-//Console.Write(number);
-//Console.Write("^2 = ");
-//Console.Write(number * number);
-Console.Write($"{number}^2 = {number * number}");*/
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+// int count = 0;
+
+// for (int i = 0; i < arr.Length; i++)
 
 // int number;
 // Console.Write("Введите число: ");
@@ -82,3 +82,93 @@ else
 2 -> 10
 */
 
+
+// ------------ Lesson 7 Двухмерные массивы
+/* ------------Задача 46:** Задайте двуменрый массив размером mxn, заполненный случайными числами.
+
+!!!Понакрутили с проверкой при печати с 3-х менрым
+m=3, n=4
+*/
+
+// int[,] array = new int[5, 3];
+
+// for (int i = 0; i < array.GetLength(0); i++) // строки
+// {
+// 	for (int j = 0; j < array.GetLength(1); j++) //столбцы
+// 	{
+// 		array[i, j] = new Random().Next(10);
+// 		Console.Write($"{array[i, j]} ");
+// 	}
+// 	Console.WriteLine();
+// }
+
+// PrintArray(array);
+
+
+// void PrintArray(Array arr)   //Для печати
+// {
+
+// 	switch (arr.Rank) // проверка на мерность массив
+// 	{
+// 		case 1:
+// 			for (int i = 0; i < arr.GetLength(0); i++) //строка одномерного массива
+// 				Console.Write($"{arr.GetValue(i)} ");
+// 			break;
+
+// 		case 2:
+// 			for (int i = 0; i < arr.GetLength(0); i++) // строки
+// 			{
+// 				for (int j = 0; j < arr.GetLength(1); j++) //столбцы
+// 				{
+// 					Console.Write($"{arr.GetValue(i, j)} ");
+// 				}
+// 				Console.WriteLine();
+// 			}
+// 			break;
+
+// 		case 3:
+// 			for (int i = 0; i < arr.GetLength(0); i++)  // строки
+// 			{
+// 				for (int j = 0; j < arr.GetLength(1); j++) //столбцы
+// 				{
+// 					for (int k = 0; k < arr.GetLength(2); k++) //3 измерение
+// 					{
+// 						Console.Write($"{arr.GetValue(i, j, k)} ");
+// 					}
+// 					Console.WriteLine();
+// 				}
+// 				Console.WriteLine();
+// 			}
+// 			break;
+// 	}
+// }
+
+/*
+Для "Зубчатых" массивов (массивов разных длинн строк), возможно использовать структуру:
+!!!! Доп вопрос
+*/
+// List<List<int>> array = new List<List<int>>();
+
+// array.Add(new List<int>());
+// array[0].Add(5);
+// array[0].Add(1);
+// array[0].Add(4);
+// array.Add(new List<int>());
+// array[1].Add(5);
+// array.Add(new List<int>());
+// array[2].Add(5);
+// array[2].Add(1);
+
+// for (int i = 0; i < array.Count; i++) //Строки
+// {
+// 	for (int j = 0; j < array[i].Count; j++) //столбцы
+// 	{
+// 		Console.Write($"{array[i][j]} ");
+// 	}
+// 	Console.WriteLine();
+// }
+
+
+/* ------------Задача :48** 
+
+*/
